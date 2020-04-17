e367f346e231920c08459bcc2f64b07ec7e5db14
xlinbin <63830116+xlinbin@users.noreply.github.com>
2020-04-17 14:12:58 +0800

    Update jump7.py

--- jump7.py
+++ jump7.py
@@ -1,11 +1,11 @@
 a = 0
 for a in range(100):
     a = a + 1
-    if a % 10 == 7:  #??a??10???7??
+    if a % 10 == 7:  #如果a除以10余数为7时
         continue
-    if a // 10 == 7: #??a??10?????7??
+    if a // 10 == 7: #如果a除以10商的整数为7时
         continue
-    if a % 7 == 0: #??a??7????0??
+    if a % 7 == 0: #如果a除以7的余数为0时
         continue
     print(a)
 
